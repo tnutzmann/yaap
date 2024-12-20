33cdde0270fedeee25bc964ce244a2f275d41ca7
tnutzmann <tonynutzmann@icloud.com>
2024-12-20 21:17:57 +0100

    feat: create Spring Boot Project

---
+++ yaap-controller/yaap-controller/src/main/java/de/tnutzmann/yaapcontroller/YaapControllerApplication.java
@@ -0,0 +1,13 @@
+package de.tnutzmann.yaapcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaapControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaapControllerApplication.class, args);
+    }
+
+}
